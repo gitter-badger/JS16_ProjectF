05101831f37eb3ff5cc56464def1a03c7cea6397
Mina Kleid <minazakiz@gmail.com>
2016-03-13 15:54:36 +0100

    injecting data as JSON to the character component

--- app/components/public/Start/Stats/CharacterThumbnail.jsx
+++ app/components/public/Start/Stats/CharacterThumbnail.jsx
@@ -4,10 +4,11 @@ import Col from 'react-bootstrap/lib/Col';
 import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 
 export default class CharacterThumbnail extends Component {
+    propTypes() { return {imageUrl: React.PropTypes.element.isRequired, name: React.PropTypes.element.isRequired};}
     render() {
         return (
             <Col md={3}>
-                <Thumbnail src={this.props.imageUrl}>
+                <Thumbnail src = {this.props.imageUrl}>
                     <p> {this.props.name}</p>
                 </Thumbnail>
             </Col>
--- app/components/public/Start/Stats/Stats.jsx
+++ app/components/public/Start/Stats/Stats.jsx
@@ -1,33 +1,51 @@
 import React from 'react';
 let {Component} = React;
 import styles from './Stats.css';
-import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Column from 'react-bootstrap/lib/Col';
 import CharacterThumbnail from './CharacterThumbnail.jsx';
 
 export default class Stats extends Component {
   render() {
+    var mostLikelyToDieComponents = this.mostLikelyToDie().map(function(character) {
+      return <CharacterThumbnail name={character.name} imageUrl={character.imageUrl}/> ;
+    });
+
+    var mostLovedOnTwitterComponents = this.mostLovedOnTwitter().map(function(character) {
+      return <CharacterThumbnail name={character.name} imageUrl={character.imageUrl}/> ;
+    });
+
     return (
       <div className={styles.stats}>
           <Row>
             <Column md={8} mdPush={2}>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
+              {mostLikelyToDieComponents}
             </Column>
           </Row>
           <Row>
             <Column md={8} mdPush={2}>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
-              <CharacterThumbnail name="test" imageUrl="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"/>
+              {mostLovedOnTwitterComponents}
             </Column>
           </Row>
 
       </div>
     );
+  }
+
+  mostLikelyToDie() {
+    return [
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"},
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"},
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"},
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"}
+    ];
+  }
+  mostLovedOnTwitter() {
+    return [
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"},
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"},
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"},
+      {name: "test", imageUrl: "http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png"}
+    ];
   }
 }
