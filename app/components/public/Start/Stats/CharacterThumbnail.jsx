004bde54f81145c622e990dc8d3e26d8ab7ef6cf
Mina Kleid <minazakiz@gmail.com>
2016-03-13 15:03:00 +0100

    fixing bootstrap columns

--- app/components/public/Start/Stats/CharacterThumbnail.jsx
+++ app/components/public/Start/Stats/CharacterThumbnail.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 let {Component} = React;
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 
 export default class CharacterThumbnail extends Component {
     render() {
         return (
-            <Grid>
-                <Row>
-                    <Col xs={6} md={3}>
-                        <Thumbnail src="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png">
-                            <h3> Character name</h3>
-                            <p> character description</p>
-                        </Thumbnail>
-                    </Col>
-                    </Row>
-                </Grid>
+            <Col md={3}>
+                <Thumbnail src="http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png">
+                    <p> Character name</p>
+                </Thumbnail>
+            </Col>
         );
     }
 }
--- app/components/public/Start/Stats/Stats.jsx
+++ app/components/public/Start/Stats/Stats.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 let {Component} = React;
 import styles from './Stats.css';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Column from 'react-bootstrap/lib/Col';
 import CharacterThumbnail from './CharacterThumbnail.jsx';
 
 export default class Stats extends Component {
   render() {
     return (
       <div className={styles.stats}>
-        <CharacterThumbnail/>
+          <Row>
+            <Column md={8} mdPush={2}>
+              <CharacterThumbnail/>
+              <CharacterThumbnail/>
+              <CharacterThumbnail/>
+              <CharacterThumbnail/>
+            </Column>
+          </Row>
+          <Row>
+            <Column md={8} mdPush={2}>
+              <CharacterThumbnail/>
+              <CharacterThumbnail/>
+              <CharacterThumbnail/>
+              <CharacterThumbnail/>
+            </Column>
+          </Row>
+
       </div>
     );
   }
