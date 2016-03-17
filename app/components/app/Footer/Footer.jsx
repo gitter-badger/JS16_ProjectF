849a852498994498b19de344d58209dbf7be7657
mammuth <kontakt@maxi-muth.de>
2016-03-17 20:15:28 +0100

    Added simple footer

--- app/components/app/Footer/Footer.jsx
+++ app/components/app/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 let {Component} = React;
-//import styles from './Footer.css';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+import styles from './Footer.css';
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <div>
-        Footer
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="footer">
+                    <div className="bottom-line">
+                        <div>2016, Rostlab @Technical Universtity Munich</div>
+                        <div className="right"><Link to={"/about"}>About &amp; Imprint</Link></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
